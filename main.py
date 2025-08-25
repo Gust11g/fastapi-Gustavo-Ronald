6736640664701a8aba44f79fad6f90971214c412
ronaldxz <ronald.santos09@aluno.ifce.edu.br>
2025-08-25 13:49:35 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI(title='API do Gustavo e do Ronald lindao')
 @app.get("/")
 def hello():
     return{"mensage": "Hello World!"}
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return{"o nome do aluno é": nome_aluno}
+
